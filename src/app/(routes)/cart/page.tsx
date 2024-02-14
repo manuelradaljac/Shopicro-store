d1fce7a6fb7c155ab365dbe0e68d208ea3a723e8
“Manuel <manuelradaljac@gmail.com>
2024-02-14 13:09:27 +0100

    manje modifikacije

--- src/app/(routes)/cart/page.tsx
+++ src/app/(routes)/cart/page.tsx
@@ -23,7 +23,7 @@ const CartPage = () => {
     <div className="">
       <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold">Vaša Košarica</h1>
+          <h1 className="text-3xl font-bold">Vaša Košarica:</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
                 {cart.items.length === 0 && <p className="text-neutral-600">Nemate proizvoda u vašoj košarici</p>}
