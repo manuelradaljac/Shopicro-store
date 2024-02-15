49ffc31fcf79e16bbb5c734d36c0b348f327ddfa
“Manuel <manuelradaljac@gmail.com>
2024-02-15 11:45:04 +0100

    Reformat - checkout error ime varijable

--- src/app/(routes)/cart/components/cart-summary.tsx
+++ src/app/(routes)/cart/components/cart-summary.tsx
@@ -32,7 +32,7 @@ const CartSummary = () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
       {
-        productId: items.map((item) => item.id),
+        productIds: items.map((item) => item.id),
       }
     );
 
