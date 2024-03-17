4a5511647436eb0e63dcd34830578e0b36aba298
Manuel Radaljac <manuelradaljac@gmail.com>
2024-03-17 12:39:05 +0100

    popravak - izbacivanje vercel analitika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "../../providers/modal-provider";
 import ToastProvider from "../../providers/toast-provider";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Head from "next/head";
 
 const font = Urbanist({ subsets: ["latin"] });
@@ -24,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-      <script defer src="https://umami-production-e3f9.up.railway.app/script.js" data-website-id="b935325a-3edb-4916-9ebe-9752a5c4fe3c"></script>
+        <script defer src="https://umami-production-e3f9.up.railway.app/script.js" data-website-id="b935325a-3edb-4916-9ebe-9752a5c4fe3c"></script>
       </Head>
       <body className={font.className}>
         <ToastProvider />
@@ -32,8 +30,6 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
-        <Analytics />
-        <SpeedInsights/>
       </body>
     </html>
   );
